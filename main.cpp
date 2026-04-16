d636b8dfcf2b2cb440e444ad20e2895e54c00144
Bennett <32626837+Bannatte@users.noreply.github.com>
2026-04-15 19:03:50 -0700

    Implement input validation and power calculation
    Added input validation for base and exponent values, and implemented power calculation.

--- main.cpp
+++ main.cpp
@@ -7,12 +7,23 @@ int main()
 
     cout << "Enter your base number: ";
     cin >> N;
+    if ((N <= 0) || (10 <= N)) {
+        return 1;
+    }
+    
     cout << "Enter your exponent: ";
     cin >> M;
+    if ((M < 1) || (10 < M)) {
+        return 1;
+    }
 
     cout << N << " to power number: ";
 
-    // TODO
+    int power = 1;
+    for (int i = 1; i <= M; i++) {
+        cout << power << " ";
+        power *= N;
+    }
 
     cout << endl;
 
